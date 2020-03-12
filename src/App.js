2c5ed0ed7351138cd20f39f6a7955325fe7391b5
AushaL <thetingler85@gmail.com>
2020-03-12 16:35:46 +0300

    add message component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import AppAside from './components/Navbar/App-aside';
 import UserProfile from './components/Profile/User-profile';
+import Messages from './components/Messages/Messages'
 
 
 const App = () => {
@@ -10,7 +11,10 @@ const App = () => {
     <div className="app-wrapper">
       <Header />
       <AppAside />
-      <UserProfile />
+      {/* <UserProfile /> */}
+      <div className='app-wrapper-content'>
+        <Messages />
+      </div>
     </div>
   );
 }
---
+++ src/components/Messages/Messages.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Messages.module.css';
+
+const Messages = (props) => {
+    return (
+        <div></div>
+    )
+}
+
+export default Messages;
